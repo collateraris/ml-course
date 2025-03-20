512ba162a15205c63eff7dc043a3afe62b7acece
i.shapturov <iulian.shapturov@my.games>
2025-03-20 09:49:27 +0300

    + распределение лапласса

--- 23s_dd_ml/homeworks/hw02_laplace/distribution.py
+++ 23s_dd_ml/homeworks/hw02_laplace/distribution.py
@@ -1,6 +1,8 @@
 import numpy as np
+from scipy.constants import value
 
-class LaplaceDistribution:    
+
+class LaplaceDistribution:
     @staticmethod
     def mean_abs_deviation_from_median(x: np.ndarray):
         '''
@@ -12,6 +14,10 @@ class LaplaceDistribution:
         # Do not change the class outside of this block
         # Your code here
         ####
+        n = len(x)
+        loc = np.median(x)
+        sum = np.sum(np.abs(x - loc))
+        return sum / n
 
     def __init__(self, features):
         '''
@@ -19,9 +25,17 @@ class LaplaceDistribution:
             feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
         '''
         ####
-        # Do not change the class outside of this block
-        self.loc = # YOUR CODE HERE
-        self.scale = # YOUR CODE HERE
+        x = np.array(features)
+
+        if x.ndim == 1:
+            self.loc = np.median(features)
+            self.scale = self.mean_abs_deviation_from_median(features)
+        elif x.ndim == 2:
+            self.loc = []  # YOUR CODE HERE
+            self.scale = []
+            for i in range(features.shape[1]):
+                self.loc.append(np.median(features[:, i]))
+                self.scale.append(self.mean_abs_deviation_from_median(features[:, i]))
         ####
 
 
@@ -33,7 +47,20 @@ class LaplaceDistribution:
         '''
         ####
         # Do not change the class outside of this block
-        return 
+        x = np.array(values)
+
+        if x.ndim == 1:
+            logpdf_val = []
+            for i in range(x.shape[0]):
+                return logpdf_val.append(-np.log(1 / (2 * self.scale)) -np.abs(values[i] - self.loc)/ self.scale)
+        elif x.ndim == 2:
+            logpdf_val = []
+            for i in range(x.shape[0]):
+                logpdf_val1 = []
+                for j in range(2):
+                    logpdf_val1.append(-np.log(2 * self.scale[j]) - np.abs(values[i, j] - self.loc[j])/ self.scale[j])
+                logpdf_val.append(logpdf_val1)
+            return logpdf_val
         ####
         
     
@@ -43,4 +70,4 @@ class LaplaceDistribution:
         Args:
             values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
         '''
-        return np.exp(self.logpdf(value))
+        return np.exp(self.logpdf(values))
